8729898059a51e7c1877c244d31ca38457fa9900
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2024-07-02 19:11:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,36 @@
-let words = document.querySelectorAll(".word");
-words.forEach((word) =>{
-    let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    });
-});
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-let curentWordIndex =0;
-let maxWordIndex = words.length -1;
-words[curentWordIndex].style.opacity = "1";
-
-let changeText = ()=>{
-    let currentWord = words[curentWordIndex];
-    let nextWord = curentWordIndex === maxWordIndex ? words[0] : words[curentWordIndex + 1];
-
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
-            letter.className = "letter out";
-        },i * 80);
-    });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter, i)=>{
-        letter.className = "letter behind";
-        setTimeout(()=>{
-            letter.className = "letter in";
-        },340 + i * 80);
-    });
-    curentWordIndex = curentWordIndex === maxWordIndex ? 0 : curentWordIndex + 1;
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
 
-changeText();
-setInterval(changeText, 3000)
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
-// Activate menu ////////////////////////////
-let menuLi = document.querySelectorAll('header ul li a');
-let section = document.querySelectorAll('section');
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-function activeMenu(){
-    let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
-    menuLi.forEach(sec => sec.classList.remove("active"));
-    menuLi[len].classList.add("active");
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window,scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+
 }
 
-activeMenu();
-window.addEventListener("scroll", activeMenu);
-
-// sticky navbar ////////////////////////////
-const header = document.querySelector("header");
-window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky",window.scrollY > 50)
-})
-
-// toggle icon navbar ////////////////////////////
-
-let menuIcon = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".navlist");
-
-menuIcon.onclick = ()=>{
-    menuIcon.classList.toggle("bx-x");
-    navlist.classList.toggle("open");
-}
-
-window.onscroll = ()=>{
-    menuIcon.classList.remove("bx-x");
-    navlist.classList.remove("open");
-}
